c83d804044af686e82c5bdb1c76fa1c54044a20f
Magomed <magomed.oziev2002@gmail.com>
2024-09-18 21:08:35 +0300

    second change

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var message string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, %s!", message)
+}
+
+func UpdateMessageHandler(w http.ResponseWriter, r *http.Request) {
+	var reqBody requestBody
+
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		http.Error(w, "Invalid request Body", http.StatusBadRequest)
+		return
+	}
+
+	message = reqBody.Message
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/message", UpdateMessageHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
